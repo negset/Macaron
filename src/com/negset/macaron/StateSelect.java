702682eb22507e7e9b17a29bc04fad8d1c248220
negset <negset1858@gmail.com>
2016-04-29 18:51:57 +0900

    リファクタリング

--- src/com/negset/macaron/StateSelect.java
+++ src/com/negset/macaron/StateSelect.java
@@ -6,7 +6,6 @@ import java.io.FilenameFilter;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
