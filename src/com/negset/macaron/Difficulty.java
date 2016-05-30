ed65e67566d80a8dd761661094c6eee86c1e1938
negset <negset1858@gmail.com>
2016-05-30 13:15:30 +0900

    リファクタリング

--- src/com/negset/macaron/Difficulty.java
+++ src/com/negset/macaron/Difficulty.java
@@ -1,5 +1,10 @@
 package com.negset.macaron;
 
+/**
+ * 難易度を表す列挙
+ *
+ * @author negset
+ */
 public enum Difficulty
 {
 	EASY("#EASY"),
@@ -9,11 +14,21 @@ public enum Difficulty
 
 	private String command;
 
+	/**
+	 * コンストラクタ
+	 *
+	 * @param command 譜面内での開始命令
+	 */
 	Difficulty(String command)
 	{
 		this.command = command;
 	}
 
+	/**
+	 * 譜面内での開始命令を返す
+	 *
+	 * @return 開始命令
+	 */
 	public String getCommand()
 	{
 		return command;
